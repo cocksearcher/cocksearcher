5db4d15014477bf81fdfaf19ba4e06c96a02dea1
KIM JUNG BIN <kjbin0420@naver.com>
2022-05-16 20:24:56 +0900

    feat: cocktaildb 칵테일 목록 가져오는 infrastructure 추가 (#5)

--- cocksearcher/infrastructures/cocktail_db.py
+++ cocksearcher/infrastructures/cocktail_db.py
@@ -1,14 +1,25 @@
 import requests
 
-BASE_URL = "www.thecocktaildb.com/api/json/v1/1/search.php"
+from cocksearcher.infrastructures.dto.cocktail_db import CocktailData
+
+BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php"
 
 
-def get_cocktail_list_by_alphabet(alphabet: str):
+def get_cocktail_list_by_alphabet(alphabet: str) -> list[CocktailData]:
     response = requests.get(BASE_URL, params=dict(f=alphabet))
-    response.json()
+    data = response.json()
+    drinks = data["drinks"]
+
+    if drinks is None:
+        return []
+
+    return [CocktailData.from_json(drink) for drink in drinks]
 
 
-def get_cocktail_list():
-    # loop by ascii letters
+def get_cocktail_list() -> list[CocktailData]:
+    cocktail_list = []
+
     for letter in (chr(i) for i in range(97, 122)):
-        get_cocktail_list_by_alphabet(letter)
+        cocktail_list.extend(get_cocktail_list_by_alphabet(letter))
+
+    return cocktail_list
